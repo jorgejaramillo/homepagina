8e8012d6ff42b87e67fb9647830aaa03dbf2573b
Jorge Jaramillo <mail@jorgejaramillo.com>
2012-01-26 13:11:10 -0500

    agrego3

---
+++ agregarcliente.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le styles -->
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/style.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+      }
+    </style>
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+
+    <div class="topbar">
+      <div class="fill">
+        <div class="container">
+          <a class="brand" href="#">Project name</a>
+          <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+	<div class="span8">
+<h2>Información <small>básica del cliente.</small></h2>
+<div class="well">
+	<h3>Nombre</h3>
+	<span class="help-block">Ejemplo: Deelf</span>
+	<input class="xlarge" id="xlInput" name="xlInput" size="30" type="text">
+	<h3>Url/Sitio web</h3>
+	<span class="help-block">Ejemplo http://deelf.com</span>
+	<input class="xlarge" id="xlInput" name="xlInput" size="30" type="text">
+	<h3>Email</h3>
+	<span class="help-block">Ejemplo hola@deelf.com</span>
+	<input class="xlarge" id="xlInput" name="xlInput" size="30" type="text">
+	
+	<h3>Teléfono</h3>
+	<span class="help-block">El telefono de la empresa.</span>
+	<input class="xlarge" id="xlInput" name="xlInput" size="30" type="text">
+	<h3>Comentarios</h3>
+	<span class="help-block">
+                Algo que decir con respecto al cliente.
+              </span>
+	<div class="clearfix">
+            <div class="input">
+              <textarea class="xlarge" id="textarea2" name="textarea2" rows="3"></textarea>
+               </div>
+          </div><br>
+	<br><div class="page-header">
+	<h3>Encargado <small>Basic templates to create webpages</small></h3>
+	</div>
+	<input class="xlarge" id="xlInput" name="xlInput" size="30" type="text"> 
+	<h3>Celular</h3>
+	 <span class="help-block">El celular del encargado</span>
+	<input class="xlarge" id="xlInput" name="xlInput" size="30" type="text">
+	<a href="#">+ Agregar Encargado</a>	
+	
+	</div>
+
+      </div>
+	<div class="span8">
+	<h2>Servicios <small>lista de servicios</small></h2>
+	<div class="well">
+	<div class="row">
+	<div class="span2"><ul class="media-grid">
+        <li>
+          <a href="#">
+            <img class="thumbnail" src="http://placehold.it/90x90" alt="">
+          </a>
+        </li>
+        <li>
+        </ul></div><div class="span4"><h4>Posicionamiento Web (SEO)</h4>
+	<p>Servicio de SEO | inicio <strong>12 de mayo de 2011</strong></p> 
+	<strong>
+	</div>
+	</div></div>
+	</div>
+      </div>
+asdf
+<div class="well" style="padding: 16px 19px;">
+    Logos 
+      </div>
+
+    </div> <!-- /container -->
+<div class="blackZone"> <div class="container">Soy Dark
+</div>
+</div>
+    <div class="container">
+ <footer>
+        <p>&copy; Company 2012</p>
+      </footer><!-- /container -->
+	</div>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bootstrap, from Twitter</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le styles -->
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="../style.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+      }
+    </style>
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+
+    <div class="topbar">
+      <div class="fill">
+        <div class="container">
+          <a class="brand" href="#">Project name</a>
+          <ul class="nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <!-- Example row of columns -->
+      <div class="row" id="login">
+	a
+      </div>
+<div class="well" style="padding: 16px 19px;">
+    Logos 
+      </div>
+
+    </div> <!-- /container -->
+<div class="blackZone"> <div class="container">Soy Dark
+</div>
+</div>
+    <div class="container">
+ <footer>
+        <p>&copy; Company 2012</p>
+      </footer><!-- /container -->
+	</div>
+  </body>
+</html>
